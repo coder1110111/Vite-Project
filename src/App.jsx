b204aba47f6cd2a96047aeb9b34570c51a3f8514
coder1110111 <ayush.yadav.dev610>
2025-07-14 09:31:21 +0530

    Learning React; Added first simple custom Component

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import ExpenseItem from './components/ExpenseItem'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Checking out VITE</h1>
+      <p>We are Learning React</p>
+      <ExpenseItem />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,10 @@
+function ExpenseItem() {
+    //When returning more than one element it is necessary to enclose them
+    return (
+        <>                                          
+            <p> Food - $5</p>
+        </>
+    )
+}
+
+export default ExpenseItem;
