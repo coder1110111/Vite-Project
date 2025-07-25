514e4fd1f9054bb3e4ce53409ae5cfbb69833e7f
coder1110111 <ayush.yadav.dev610>
2025-07-25 09:39:05 +0530

    This is how Multiple to one state is used in a Form

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+
+
+import React, { StrictMode } from 'react';
 import {useState} from 'react';
 import './ExpenseForm.css';
 
@@ -8,18 +10,58 @@ function ExpenseForm() {
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
 
+    
+    /* How multiple single state are
+    // const titleChangeHandler = (event) => {
+    //     setEnteredTitle(event.target.value);
+    // }
+
+    // const amountChangeHandler = (event) => {
+    //     setEnteredAmount(event.target.value);
+    // }
+
+    // const dateChangeHandler = (event) => {
+    //     setEnteredDate(event.target.value);
+    // }
+
+    */
+
+    //Multiple to One State
+    const [userInput, setUserInput] = useState({
+        enteredTitle:'',
+        enteredAmount: '',
+        enteredDate: ''
+    });
+
     const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value);
-    }
+        setUserInput((prevState) => {
+            return {
+                ...prevState,
+                enteredTitle: event.target.value
+            }
+        });
+    };
 
     const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
-    }
+        setUserInput((prevState) => {
+            return {
+                ...prevState,
+                enteredAmount: event.target.value
+            }
+        });
+    };
 
     const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
-    }
+        setUserInput((prevState) => {
+            return {
+                ...prevState,
+                enteredDate: event.target.value
+            }
+        });
+    };
 
+    
+    
     return <form>
         <div className='new-expense__controls'>
             <div className='new_expense__control'>
