9e8400ae6565686cb0b7628d153ac9d8e6365fa6
coder1110111 <ayush.yadav.dev610>
2025-07-25 08:44:07 +0530

    Example how multiple single state are

--- src/components/NewExpense/ExpenseForm.jsx
+++ src/components/NewExpense/ExpenseForm.jsx
@@ -1,20 +1,38 @@
 import React from 'react';
+import {useState} from 'react';
 import './ExpenseForm.css';
 
 function ExpenseForm() {
+
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    }
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    }
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    }
+
     return <form>
         <div className='new-expense__controls'>
             <div className='new_expense__control'>
                 <label>Title : </label>
-                <input type='text' />
+                <input type='text' onChange={titleChangeHandler} />
             </div>
             <div className='new_expense__control'>
                 <label>Amount : </label>
-                <input type='number' />
+                <input type='number' onChange={amountChangeHandler} />
             </div>
             <div className='new_expense__control'>
                 <label>Date : </label>
-                <input type='date' min="2023-01-01" max="2024-12-31" />
+                <input type='date' onChange={dateChangeHandler} min="2023-01-01" max="2024-12-31" />
             </div>
         </div>
         <div className='new-expense__actions'>
